ab4bca2af88d77f5ba7ba2d64c5dfc352d92e561
Lev Oleh <oleg.levchenko.topdevs@gmail.com>
2020-06-23 16:07:37 +0300

    LAW-15 Register Clients - login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,14 +3,45 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 const config = require('../config/appSettings');
-const {catchResponseHandler} = require('../helpers/http');
+const {catchResponseHandler,notFoundResponseHandler} = require('../helpers/http');
 const {getSMSCode} = require('../helpers/func');
 
-router.get('/', async (req, res) => {
+router.post('/login', async (req, res) => {
+    try {
+        const {phone, password} = req.body;
+        const user = await User.findOne({phone});
 
-    res.json({
-        data: 'OK'
-    })
+        if (!user) return notFoundResponseHandler(res,'User not found');
+        if ( bcrypt.compareSync(password, user.password) ) {
+            const token = jwt.sign(
+                {phone: user.phone},
+                config.jwtSalt,
+                { expiresIn: +config.expiresIn * 1000 }
+            );
+
+            user.phone_verified_at = Date.now();
+            user.sms_code = null;
+            user.save();
+
+            return res.json({
+                token
+            })
+        }else {
+            res.status(401).json({
+                errors: [
+                    {
+                        status: 401,
+                        title: 'Login failed! Check authentication credentials'
+                    }
+                ]
+            });
+        }
+
+    } catch (e) {
+        // todo logger ERROR
+        console.log(e);
+        return catchResponseHandler(res, "Can't login current user");
+    }
 });
 
 router.post('/register', async (req, res) => {
@@ -33,23 +64,13 @@ router.post('/register', async (req, res) => {
         return catchResponseHandler(res, "Can't create new user");
     }
 });
+
 router.post('/activate', async (req, res) => {
     try {
         const {sms_code} = req.body;
-        const user = await User.findOne({
-            sms_code,
-        });
+        const user = await User.findOne({sms_code});
 
-        if (!user) {
-            return res.status(404).json({
-                errors: [
-                    {
-                        status: "404",
-                        title:  "User not found",
-                    }
-                ]
-            });
-        }
+        if (!user) return notFoundResponseHandler(res,'User not found');
         const token = jwt.sign(
             {phone: user.phone},
             config.jwtSalt,
--- server/helpers/http.js
+++ server/helpers/http.js
@@ -9,3 +9,13 @@ exports.catchResponseHandler = (res,errMsg,code=500) => {
         ]
     });
 };
+exports.notFoundResponseHandler = (res,errMsg,code=404) => {
+    return res.status(code).json({
+        errors: [
+            {
+                status: code,
+                title:  errMsg,
+            }
+        ]
+    });
+};
