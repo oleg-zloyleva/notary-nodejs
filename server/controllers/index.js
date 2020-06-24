6145de000e6bc3977c1bc868aba7a9d1bd95f768
Lev Oleh <oleg.levchenko.topdevs@gmail.com>
2020-06-24 17:12:42 +0300

    LAW-15 Register Clients - change phone

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,5 +3,6 @@ const apiPrefix = require('../config/appSettings').apiPrefix;
 
 router.use(`${apiPrefix}/auth`, require('./auth'));
 router.use(`${apiPrefix}/password`, require('./password'));
+router.use(`${apiPrefix}/phone`, require('./phone'));
 
 module.exports = router;
