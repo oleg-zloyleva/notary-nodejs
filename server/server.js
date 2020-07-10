9fd81808090c80618871c266bf4e04199f26b351
Lev Oleh <oleg.levchenko.topdevs@gmail.com>
2020-07-10 14:16:41 +0300

    change atach screen logic

--- server/server.js
+++ server/server.js
@@ -17,37 +17,37 @@ mongo.run();
 app.use(express.json());
 
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'REST API for notary service',
-        version: '1.0.0',
-        description: 'This is the REST API for notary service',
+  openapi: '3.0.0',
+  info: {
+    title: 'REST API for notary service',
+    version: '1.0.0',
+    description: 'This is the REST API for notary service',
+  },
+  components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
     },
-    components: {
-        securitySchemes: {
-            BearerAuth: {
-                type: 'http',
-                scheme: 'bearer',
-                bearerFormat: 'JWT',
-            }
-        },
-    },
+  },
 };
 
 // options for the swagger docs
 const options = {
-    // import swaggerDefinitions
-    swaggerDefinition,
-    // path to the API docs
-    apis: ['./swagger/**/*.yaml'],
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./swagger/**/*.yaml'],
 };
 // initialize swagger-jsdoc
 const swaggerSpec = swaggerJSDoc(options);
 const swaggerSetup = {
-    ...swaggerSpec,
-    servers: [
-        {url: '/api'}
-    ]
+  ...swaggerSpec,
+  servers: [
+    { url: '/api' },
+  ],
 };
 app.use(`${config.apiPrefix}/doc`, swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
@@ -60,5 +60,5 @@ app.use(ErrorMiddleWare.MongoErrorMiddleware);
 app.use(ErrorMiddleWare.GeneralErrorMiddleware);
 
 app.listen(config.port, () => {
-    console.log(`App was started at ${config.port} port. And connected to Mongo`.white.bgCyan);
+  console.log(`App was started at ${config.port} port. And connected to Mongo`.white.bgCyan);
 });
