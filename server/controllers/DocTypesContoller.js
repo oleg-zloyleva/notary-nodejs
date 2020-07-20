17b652f3423d43b3a9338e8074101ec11ab1003a
Lev Oleh <oleg.levchenko.topdevs@gmail.com>
2020-07-20 13:12:55 +0300

    create docs List

---
+++ server/controllers/DocTypesContoller.js
@@ -0,0 +1,60 @@
+const docTypeHandlers = (modelName) => {
+  const docType = require(`../models/${modelName}`);
+
+  const findAll = async (req, res, next) => {
+    try {
+      const data = await docType.getAll(req);
+      return res.json({ data });
+    } catch (e) {
+      return next(e);
+    }
+  };
+
+  const findOne = async (req, res, next) => {
+    try {
+      const data = await docType.getOne(req);
+      return res.json({ data });
+    } catch (e) {
+      return next(e);
+    }
+  };
+
+  const createOne = async (req, res, next) => {
+    try {
+      const data = await docType.createOne(req);
+      return res.json({ data });
+    } catch (e) {
+      return next(e);
+    }
+  };
+
+  const uploadScreens = async (req, res, next) => {
+    try {
+      const document = await docType.uploadScreens(req);
+      return res.json({
+        data: document,
+      });
+    } catch (e) {
+      return next(e);
+    }
+  };
+
+  const sendToCheck = async (req, res, next) => {
+    try {
+      const data = await docType.sendToCheck(req);
+      return res.json({ data });
+    } catch (e) {
+      return next(e);
+    }
+  };
+
+  return {
+    findAll,
+    findOne,
+    createOne,
+    uploadScreens,
+    sendToCheck,
+  };
+};
+
+module.exports = docTypeHandlers;
--- server/controllers/ScreenImageController/screenImage.router.js
+++ server/controllers/ScreenImageController/screenImage.router.js
@@ -3,5 +3,6 @@ const ScreenImageController = require('./screenImage');
 const auth = require('../../middlewares/auth');
 
 router.get('/:id', auth, ScreenImageController.findOne);
+// router.delete('/:id', auth, ScreenImageController.removeOne);
 
 module.exports = router;
