98bef8fb6fb0ed68c166d9fa708108edba53d505
Lev Oleh <oleg.levchenko.topdevs@gmail.com>
2020-07-28 18:08:39 +0300

    LAW-32/Create_doc_type_F

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -11,6 +11,7 @@ router.use(`${apiPrefix}/type_b`, require('./docTypeBController/docTypeB.router'
 router.use(`${apiPrefix}/type_c`, require('./docTypeCController/docTypeC.router'));
 router.use(`${apiPrefix}/type_d`, require('./docTypeDController/docTypeD.router'));
 router.use(`${apiPrefix}/type_e`, require('./docTypeEController/docTypeE.router'));
+router.use(`${apiPrefix}/type_f`, require('./docTypeFController/docTypeF.router'));
 
 router.use(`${apiPrefix}/screens`, require('./ScreenImageController/screenImage.router'));
 router.use(`${apiPrefix}/docs`, require('./docsListController/docsList.router'));
