9a332eca03c163783d6711cd8ee15e19c6c014da
Lev Oleh <oleg.levchenko.topdevs@gmail.com>
2020-08-13 14:57:19 +0300

    add deps

---
+++ client/src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ContainerWrapper = styled.div`
+  margin: 0 auto;
+  width: 1280px;
+`;
+
+function App() {
+  return (
+    <ContainerWrapper>
+      text
+    </ContainerWrapper>
+  );
+}
+
+export { App };
